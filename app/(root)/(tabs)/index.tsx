06f0e32f31938848ae1eca837658429c35debaa8
Júlio António <julioantonio1408@gmail.com>
2025-01-06 06:21:09 +0100

    refresh

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,52 +1,122 @@
 import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Link } from "expo-router";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-povider";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { Link, router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import {
+    FlatList,
+    Image,
+    ScrollView,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  return (
-    <SafeAreaView className="bg-white h-full">
-      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName='pb-20'>
-        <View className="px-5">
-          <View className="flex flex-row items-center justify-between mt-5">
-            <View className="flex flex-row items-center">
-              <Image source={images.avatar} className="size-12 rounded-full" />
-              <View className="flex flex-col items-start ml-2 justify-center">
-                <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-                <Text className="text-base font-rubik-medium text-black-300">Adrien</Text>
-              </View>
-            </View>
-            <Image source={icons.bell} className="size-6" />
-          </View>
-          <Search />
-          <View className="my-4">
-            <View className="flex flex-row items-center justify-between">
-              <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
-              <TouchableOpacity>
-                <Text className="text-base font-rubik text-primary-300">See All</Text>
-              </TouchableOpacity>
-            </View>
-            <View className="flex flex-row gap-5 mt-4">
-              <FeaturedCard />
-              <FeaturedCard />
-            </View>
-          </View>
-          <View className="flex flex-row items-center justify-between">
-            <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
-            <TouchableOpacity>
-              <Text className="text-base font-rubik text-primary-300">See All</Text>
-            </TouchableOpacity>
-          </View>
+    const { user } = useGlobalContext();
+    const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+    const { data: latestProperties, loading: latestPropertiesLoading } =
+        useAppwrite({
+            fn: getLatestProperties,
+        });
 
-          <View className="flex flex-row gap-5 mt-4">
-            <Card />
-            <Card />
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    const {
+        data: properties,
+        loading,
+        refetch,
+    } = useAppwrite({
+        fn: getProperties,
+        params: {
+            filter: params.filter!,
+            query: params.query!,
+            limit: 6,
+        },
+        skip: true,
+    });
+    
+    const handleCardPress = (id:string) => router.push(`/properties/${id}`);
+
+    useEffect(() => {
+        refetch({
+            filter: params.filter!,
+            query: params.query!,
+            limit: 6,
+        });
+    }, [params.filter, params.query]);
+
+    return (
+        <SafeAreaView className="bg-white h-full">
+            <FlatList
+                data={properties}
+                renderItem={({ item }) => <Card item={item} onPress={()=> handleCardPress(item.$id)} />}
+                keyExtractor={(item) => item.toString()}
+                numColumns={2}
+                contentContainerClassName="pb-32"
+                columnWrapperClassName="flex gap-5 px-5"
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View className="px-5">
+                        <View className="flex flex-row items-center justify-between mt-5">
+                            <View className="flex flex-row items-center">
+                                <Image
+                                    source={{ uri: user?.avatar }}
+                                    className="size-12 rounded-full"
+                                />
+                                <View className="flex flex-col items-start ml-2 justify-center">
+                                    <Text className="text-xs font-rubik text-black-100">
+                                        Good Morning
+                                    </Text>
+                                    <Text className="text-base font-rubik-medium text-black-300">
+                                        {user?.name}
+                                    </Text>
+                                </View>
+                            </View>
+                            <Image source={icons.bell} className="size-6" />
+                        </View>
+                        <Search />
+                        <View className="my-4">
+                            <View className="flex flex-row items-center justify-between">
+                                <Text className="text-xl font-rubik-bold text-black-300">
+                                    Featured
+                                </Text>
+                                <TouchableOpacity>
+                                    <Text className="text-base font-rubik text-primary-300">
+                                        See All
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+
+                            <FlatList
+                                data={latestProperties}
+                                renderItem={(item) => <FeaturedCard item={item} onPress={()=> handleCardPress(item.$id)} />}
+                                horizontal
+                                bounces={false}
+                                showsHorizontalScrollIndicator={false}
+                                contentContainerClassName="flex gap-5 mt-4"
+                                keyExtractor={(item) => item.toString()}
+                            />
+                        </View>
+                        <View className="flex flex-row items-center justify-between">
+                            <Text className="text-xl font-rubik-bold text-black-300">
+                                Our Recommendation
+                            </Text>
+                            <TouchableOpacity>
+                                <Text className="text-base font-rubik text-primary-300">
+                                    See All
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <Filters />
+                    </View>
+                }
+            />
+        </SafeAreaView>
+    );
 }
